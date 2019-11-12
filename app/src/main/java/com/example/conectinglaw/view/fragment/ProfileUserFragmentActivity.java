7a36be5fbf9f2a0b8b362ced104d183b4525675a
Germán Arevalo <germanarevalojerez@gmail.com>
2019-11-12 18:19:04 -0500

    set photoProfile

--- app/src/main/java/com/example/conectinglaw/view/fragment/ProfileUserFragmentActivity.java
+++ app/src/main/java/com/example/conectinglaw/view/fragment/ProfileUserFragmentActivity.java
@@ -1,14 +1,20 @@
 package com.example.conectinglaw.view.fragment;
 
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.conectinglaw.R;
@@ -17,13 +23,22 @@ import com.example.conectinglaw.model.Lawyer;
 import com.example.conectinglaw.model.User;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.app.Activity.RESULT_OK;
+
 public class ProfileUserFragmentActivity extends Fragment {
 
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
     private String TAG = "ProfileUserFragmentActivity";
 
     TextView txtNombreUser, txtApellidosUser, txtCedulaUser,
             txtCorreoUser, txtCelularUser, txtTipoAbogadoUser;
-    ImageButton fotoPerfilUser;
+    ImageView fotoPerfilUser;
 
     public ProfileUserFragmentActivity(){
 
@@ -64,7 +79,27 @@ public class ProfileUserFragmentActivity extends Fragment {
             txtTipoAbogadoUser.setText(((Lawyer) user).tipoAbogado());
         }
 
+        fotoPerfilUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setPicture();
+            }
+        });
+
         return view;
     }
 
+    private void setPicture() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            fotoPerfilUser.setImageURI(imageUri);
+        }
+    }
 }
